f9e6cbf17245e9a9af0c1e0805d06520a89d79fa
Mehmet Akif �rgen <urgenmakif@gmail.com>
2019-08-30 04:16:33 +0300

    first send

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.baglanButton = new System.Windows.Forms.Button();
+            this.bagkesButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.testButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.updateButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // baglanButton
+            // 
+            this.baglanButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.baglanButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baglanButton.ForeColor = System.Drawing.Color.Black;
+            this.baglanButton.Location = new System.Drawing.Point(31, 107);
+            this.baglanButton.Name = "baglanButton";
+            this.baglanButton.Padding = new System.Windows.Forms.Padding(5);
+            this.baglanButton.Size = new System.Drawing.Size(132, 70);
+            this.baglanButton.TabIndex = 0;
+            this.baglanButton.Text = "CONNECT";
+            this.baglanButton.UseVisualStyleBackColor = false;
+            this.baglanButton.Click += new System.EventHandler(this.baglanButton_Click);
+            // 
+            // bagkesButton
+            // 
+            this.bagkesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bagkesButton.Location = new System.Drawing.Point(31, 253);
+            this.bagkesButton.Name = "bagkesButton";
+            this.bagkesButton.Size = new System.Drawing.Size(132, 70);
+            this.bagkesButton.TabIndex = 1;
+            this.bagkesButton.Text = "DISCONNECT";
+            this.bagkesButton.UseVisualStyleBackColor = true;
+            this.bagkesButton.Click += new System.EventHandler(this.bagkesButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(132, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(551, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 121);
+            this.textBox1.TabIndex = 3;
+            // 
+            // testButton
+            // 
+            this.testButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.testButton.Location = new System.Drawing.Point(551, 208);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(130, 44);
+            this.testButton.TabIndex = 4;
+            this.testButton.Text = "SEND";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(548, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.ForeColor = System.Drawing.Color.Lime;
+            this.progressBar1.Location = new System.Drawing.Point(31, 208);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(132, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightGray;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(259, 148);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(206, 175);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sp_DataReceived);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.updateButton.Location = new System.Drawing.Point(551, 275);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(130, 48);
+            this.updateButton.TabIndex = 11;
+            this.updateButton.Text = "Check Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(259, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(718, 361);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.bagkesButton);
+            this.Controls.Add(this.baglanButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TextChanged += new System.EventHandler(this.DisplayText);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button baglanButton;
+        private System.Windows.Forms.Button bagkesButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.IO.Ports;// Seri Port için kütüphane
+using System.Threading;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        private string data;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            bagkesButton.Enabled = false;
+            testButton.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+        }
+       
+        private void sp_DataReceived(object sender,SerialDataReceivedEventArgs e) // sürekli veri okuması için gereken fonksiyon
+        {
+            data = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(DisplayText));
+        }
+
+
+        private void DisplayText(object sender, EventArgs e)// textBox'a yazdıran fonksiyon
+        {
+            textBox2.AppendText(data);
+            
+        }
+
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames(); //Bağlı seri portları diziye aktardık
+            foreach (string portAdi in ports)
+            {
+                comboBox1.Items.Add(portAdi);
+            }
+            if (serialPort1.IsOpen)
+            {
+                
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.ReadTimeout = 500;
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);// Read Etmek için fonksiyonumuzu sürekli çağırıyoruz.
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = comboBox1.SelectedItem.ToString(); //combobox ta seçilen seri porta seçme
+            }
+            
+        }
+
+        private void baglanButton_Click(object sender, EventArgs e)//bağlantı butonu
+        {
+            try
+            {
+                serialPort1.Open();
+                progressBar1.Value = 100;
+                
+                baglanButton.Enabled = false;
+                bagkesButton.Enabled = true;
+                testButton.Enabled = true;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            
+        }
+
+        private void bagkesButton_Click(object sender, EventArgs e)//bağlantı kesme butonu
+        {
+            try
+            {
+                serialPort1.Close();
+                progressBar1.Value = 0;
+                bagkesButton.Enabled = false;
+                baglanButton.Enabled = true;
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            
+        }
+        private void testButton_Click(object sender, EventArgs e)// yazdırma butonu
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(textBox1.Text);
+                textBox1.Clear();
+            }
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Henüz Etkin Değil");
+        }
+    }
+}
